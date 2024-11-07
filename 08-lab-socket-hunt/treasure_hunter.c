542d50ec556022f7372453088c66981e1a32387b
Cannon Farr <cf436@barricade.cs.byu.edu>
2024-11-07 16:22:37 -0700

    wip

--- 08-lab-socket-hunt/treasure_hunter.c
+++ 08-lab-socket-hunt/treasure_hunter.c
@@ -1,42 +1,118 @@
 // Replace PUT_USERID_HERE with your actual BYU CS user id, which you can find
 // by running `id -u` on a CS lab machine.
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define MAX_RESPONSE_SIZE 256
+
 #define USERID 1823700680
 
 #include <stdio.h>
 
-#include "sockhelper.h"
+#include "../include/sockhelper.h"
 
 int verbose = 0;
 
+struct response_values {
+  unsigned char chunk_length;
+  char *chunk;
+  unsigned char op_code;
+  unsigned short op_param;
+  int nonce;
+};
+
+
 void print_bytes(unsigned char *bytes, int byteslen);
+int extract_response_values(const unsigned char *response, struct response_values *values);
+void print_response_values (const struct response_values * values);
 
 int main(int argc, char *argv[]) {
-	char *server = argv[1];
-	char *port = argv[2];
-	char *level = argv[3];
-	char *seed = argv[4];
+  char *server = argv[1];
+  char *port = argv[2];
+  char *level = argv[3];
+  char *seed = argv[4];
 
-	printf("server: %s | port: %s | level: %s | seed: %s\n", server, port, level,  seed);
+  printf("server: %s | port: %s | level: %s | seed: %s\n", server, port, level,
+         seed);
 
-	int port_num = atoi(port);
-	int level_num = atoi(level);
-	unsigned short seed_num = atoi(seed);
-	seed_num = ntohs(seed_num);
-	level_num = ntohs(level_num);
-	unsigned int user_id = ntohl(USERID);
-	unsigned char init_request_body[8];
-	memset(init_request_body, 0, 8);
-	
-	memcpy(&init_request_body[1], &level_num, sizeof(int));
-	memcpy(&init_request_body[2], &user_id, sizeof(unsigned int));
-	memcpy(&init_request_body[6], &seed_num, sizeof(unsigned short));
+  int port_num = atoi(port);
+  int level_num = atoi(level);
+  unsigned short seed_num = atoi(seed);
+  seed_num = ntohs(seed_num);
+  unsigned int user_id = ntohl(USERID);
+  unsigned char init_request_body[8];
+  memset(init_request_body, 0, 8);
 
-	print_bytes(init_request_body, 8);
+  memcpy(&init_request_body[1], &level_num, sizeof(int));
+  memcpy(&init_request_body[2], &user_id, sizeof(unsigned int));
+  memcpy(&init_request_body[6], &seed_num, sizeof(unsigned short));
 
+  // print_bytes(init_request_body, 8);
 
+  // Socket setup
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(struct addrinfo));
+
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+
+  struct addrinfo *info;
+
+  int s = getaddrinfo(server, port, &hints, &info);
+  int sock = socket(info->ai_family, info->ai_socktype, 0);
+  struct sockaddr_storage remote_addr_ss;
+  struct sockaddr *remote_addr = (struct sockaddr *)&remote_addr_ss;
+
+  // Set up remote addr reference
+  printf("Setting up remote\n");
+  memcpy(remote_addr, info->ai_addr, sizeof(struct sockaddr_storage));
+  char remote_ip[INET6_ADDRSTRLEN];
+  unsigned short remote_port = 0;
+
+  printf("populating addr\n");
+  // populate_sockaddr(remote_addr, hints.ai_family, NULL, remote_port );
+  parse_sockaddr(remote_addr, remote_ip, &remote_port);
+
+  // Set up local addr reference
+  printf("Setting up local\n");
+
+  struct sockaddr_storage local_addr_ss;
+  struct sockaddr *local_addr = (struct sockaddr *)&local_addr_ss;
+
+  char local_ip[INET6_ADDRSTRLEN];
+  unsigned short local_port;
+
+  socklen_t addr_len = sizeof(struct sockaddr_storage);
+  s = getsockname(sock, local_addr, &addr_len);
+  parse_sockaddr(local_addr, local_ip, &local_port);
+
+  printf("addr info: ip: %s | port: %d\n", remote_ip, remote_port);
+  // Server Test
+
+  printf("Setting up server test\n");
+  ssize_t n_written =
+      sendto(sock, init_request_body, 8, 0, remote_addr, addr_len);
+
+  unsigned char response_buf[MAX_RESPONSE_SIZE];
+  ssize_t n_received = recvfrom(sock, response_buf, MAX_RESPONSE_SIZE, 0,
+                                remote_addr, &addr_len);
+
+  printf("Response size: %zu\n", n_received);
+  print_bytes(response_buf, n_received);
+
+  struct response_values *values = malloc(sizeof(struct response_values));
+  
+  printf("extracting\n");
+  int err = extract_response_values(response_buf, values);
+  printf("printing\n");
+
+  print_response_values(values);
+  // ******* Levels ********
 }
 
 void print_bytes(unsigned char *bytes, int byteslen) {
@@ -78,4 +154,29 @@ void print_bytes(unsigned char *bytes, int byteslen) {
   }
   printf("\n");
   fflush(stdout);
+}
+
+int extract_response_values(const unsigned char * response, struct response_values *values) {
+
+  values->chunk_length = response[0];
+  if (values->chunk_length > 127) {
+    return -1;
+  }
+
+  memcpy(&values->chunk, response + 1, (size_t)values->chunk_length);
+  values->chunk += '\0';
+  values->chunk_length += 1;
+  memcpy(&values->op_code, response + values->chunk_length + 2, 1);
+  memcpy(&values->op_param, response + values->chunk_length + 3, 2);
+  memcpy(&values->nonce, response + values->chunk_length + 5, 4);
+
+  return 0;
+}
+
+
+void print_response_values (const struct response_values * values) {
+  printf("chunk length: %d\n", values->chunk_length);
+  printf("chunk content: %s\n", values->chunk);
+  printf("op code: %x\n", values->op_code);
+  printf("op param: %x\n", values->op_param);
 }
